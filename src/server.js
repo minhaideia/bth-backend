642c69bb64128973917950683dc32bffe5e2ddc5
Minha Ideia <minhaideia.xyz@gmail.com>
2020-04-21 20:04:25 -0300

    adding port

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 const app = require('./app');
 
-app.listen(process.env.PORT || 3333);
+app.listen(process.env.PORT || 3333, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
+});
